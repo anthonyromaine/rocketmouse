1a6a69338fc8faa296e4272845ab0be5c4092a20
Lettuce05 <romaine@unlv.nevada.edu>
2023-07-24 12:55:41 -0600

    Add Rocket Mouse Character

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,24 @@
-import Phaser from 'phaser'
+import Phaser from "phaser";
 
-import Game from './scenes/Game';
+import Preloader from "./scenes/Preloader";
+import Game from "./scenes/Game";
 
 const config: Phaser.Types.Core.GameConfig = {
-	type: Phaser.AUTO,
-	parent: 'app',
-	width: 800,
-	height: 640,
+  type: Phaser.AUTO,
+  parent: "app",
+  width: 800,
+  height: 640,
   scale: {
-    mode: Phaser.Scale.ScaleModes.FIT
+    mode: Phaser.Scale.ScaleModes.FIT,
   },
   autoCenter: Phaser.Scale.Center.CENTER_BOTH,
-	physics: {
-		default: 'arcade',
-		arcade: {
-			gravity: { y: 200 },
-		},
-	},
-	scene: [Game],
-}
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 200 },
+    },
+  },
+  scene: [Preloader, Game],
+};
 
-export default new Phaser.Game(config)
+export default new Phaser.Game(config);
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,17 +1,29 @@
 import Phaser from "phaser";
+import TextureKeys from "../consts/TextureKeys";
+import SceneKeys from "../consts/SceneKeys";
+import AnimationKeys from "../consts/AnimationKeys";
 
 export default class Game extends Phaser.Scene {
-    constructor () {
-        super('game');
-    }
+  constructor() {
+    super(SceneKeys.Game);
+  }
 
-    preload() {
-        this.load.image('background', 'house/bg_repeat_340x640.png');
-    }
+  preload() {}
 
-    create() {
-        const width = this.scale.width;
-        const height = this.scale.height;
-        this.add.tileSprite(0, 0, width, height, 'background').setOrigin(0);
-    }
+  create() {
+    const width = this.scale.width;
+    const height = this.scale.height;
+    this.add
+      .tileSprite(0, 0, width, height, TextureKeys.Background)
+      .setOrigin(0);
+
+    this.add
+      .sprite(
+        width * 0.5,
+        height * 0.5,
+        TextureKeys.RocketMouse,
+        "rocketmouse_fly01.png",
+      )
+      .play(AnimationKeys.RocketMouseRun);
+  }
 }
