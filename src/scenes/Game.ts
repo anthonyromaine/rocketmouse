32d530fa4cd81d7496b08754071d85cd99dbb2e2
Lettuce05 <romaine@unlv.nevada.edu>
2023-07-24 14:02:50 -0600

    Add windows and bookcases to the decor of the background

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -6,6 +6,13 @@ import AnimationKeys from "../consts/AnimationKeys";
 export default class Game extends Phaser.Scene {
   private background!: Phaser.GameObjects.TileSprite;
   private mouseHole!: Phaser.GameObjects.Image;
+  private window1!: Phaser.GameObjects.Image;
+  private window2!: Phaser.GameObjects.Image;
+  private bookcase1!: Phaser.GameObjects.Image;
+  private bookcase2!: Phaser.GameObjects.Image;
+
+  private bookcases: Phaser.GameObjects.Image[] = [];
+  private windows: Phaser.GameObjects.Image[] = [];
 
   constructor() {
     super(SceneKeys.Game);
@@ -16,17 +23,47 @@ export default class Game extends Phaser.Scene {
   create() {
     const width = this.scale.width;
     const height = this.scale.height;
+
+    // create background
     this.background = this.add
       .tileSprite(0, 0, width, height, TextureKeys.Background)
       .setOrigin(0)
       .setScrollFactor(0, 0);
 
+    // create mouse hole
     this.mouseHole = this.add.image(
       Phaser.Math.Between(900, 1500),
       501,
       TextureKeys.MouseHole,
     );
 
+    // create windows
+    this.window1 = this.add.image(
+      Phaser.Math.Between(900, 1300),
+      200,
+      TextureKeys.Window1,
+    );
+
+    this.window2 = this.add.image(
+      Phaser.Math.Between(1600, 2000),
+      200,
+      TextureKeys.Window2,
+    );
+
+    this.windows = [this.window1, this.window2];
+
+    // create bookcases
+    this.bookcase1 = this.add
+      .image(Phaser.Math.Between(2200, 2700), 580, TextureKeys.Bookcase1)
+      .setOrigin(0.5, 1);
+
+    this.bookcase2 = this.add
+      .image(Phaser.Math.Between(2900, 3400), 580, TextureKeys.Bookcase2)
+      .setOrigin(0.5, 1);
+
+    this.bookcases = [this.bookcase1, this.bookcase2];
+
+    // create mouse
     const mouse = this.physics.add
       .sprite(
         width * 0.5,
@@ -49,6 +86,8 @@ export default class Game extends Phaser.Scene {
 
   update(time: number, delta: number): void {
     this.wrapMouseHole();
+    this.wrapWindows();
+    this.wrapBookcases();
 
     this.background.setTilePosition(this.cameras.main.scrollX);
   }
@@ -59,6 +98,78 @@ export default class Game extends Phaser.Scene {
 
     if (this.mouseHole.x + this.mouseHole.width < scrollX) {
       this.mouseHole.x = Phaser.Math.Between(rightEdge + 100, rightEdge + 1000);
+    }
+  }
+
+  private wrapWindows() {
+    const scrollX = this.cameras.main.scrollX;
+    const rightEdge = scrollX + this.scale.width;
+
+    // multiply by 2 to add some padding
+    let width = this.window1.width * 2;
+    if (this.window1.x + width < scrollX) {
+      this.window1.x = Phaser.Math.Between(rightEdge + width, rightEdge + 800);
+
+      const overlap = this.bookcases.find((bc) => {
+        return Math.abs(this.window1.x - bc.x) <= this.window1.width;
+      });
+
+      // show window if there is no overlap
+      // otherwise hide window
+      this.window1.visible = !overlap;
+    }
+
+    width = this.window2.width;
+    if (this.window2.x + width < scrollX) {
+      this.window2.x = Phaser.Math.Between(
+        this.window1.x + width,
+        this.window1.x + width + 800,
+      );
+
+      const overlap = this.bookcases.find((bc) => {
+        return Math.abs(this.window2.x - bc.x) <= this.window2.width;
+      });
+
+      // show window if there is no overlap
+      // otherwise hide window
+      this.window2.visible = !overlap;
+    }
+  }
+
+  private wrapBookcases() {
+    const scrollX = this.cameras.main.scrollX;
+    const rightEdge = scrollX + this.scale.width;
+
+    let width = this.bookcase1.width * 2;
+    if (this.bookcase1.x + width < scrollX) {
+      this.bookcase1.x = Phaser.Math.Between(
+        rightEdge + width,
+        rightEdge + width + 800,
+      );
+
+      const overlap = this.windows.find((win) => {
+        return Math.abs(this.bookcase1.x - win.x) <= win.width;
+      });
+
+      // show bookcase if there is no overlap
+      // otherwise hide bookcase
+      this.bookcase1.visible = !overlap;
+    }
+
+    width = this.bookcase2.width;
+    if (this.bookcase2.x + width < scrollX) {
+      this.bookcase2.x = Phaser.Math.Between(
+        this.bookcase1.x + width,
+        this.bookcase1.x + width + 800,
+      );
+
+      const overlap = this.windows.find((win) => {
+        return Math.abs(this.bookcase2.x - win.x) <= win.width;
+      });
+
+      // show bookcase if there is no overlap
+      // otherwise hide bookcase
+      this.bookcase2.visible = !overlap;
     }
   }
 }
--- src/scenes/Preloader.ts
+++ src/scenes/Preloader.ts
@@ -9,9 +9,21 @@ export default class Preloader extends Phaser.Scene {
   }
 
   preload() {
+    // load background
     this.load.image(TextureKeys.Background, "house/bg_repeat_340x640.png");
 
+    // load mouse hole
     this.load.image(TextureKeys.MouseHole, "house/object_mousehole.png");
+
+    // load windows
+    this.load.image(TextureKeys.Window1, "house/object_window1.png");
+    this.load.image(TextureKeys.Window2, "house/object_window2.png");
+
+    // load bookcases
+    this.load.image(TextureKeys.Bookcase1, "house/object_bookcase1.png");
+    this.load.image(TextureKeys.Bookcase2, "house/object_bookcase2.png");
+
+    // load rocket mouse animation sprite sheet
     this.load.atlas(
       TextureKeys.RocketMouse,
       "characters/rocket-mouse.png",
@@ -20,6 +32,7 @@ export default class Preloader extends Phaser.Scene {
   }
 
   create() {
+    // create run animation
     this.anims.create({
       key: AnimationKeys.RocketMouseRun,
       frames: this.anims.generateFrameNames(TextureKeys.RocketMouse, {
@@ -33,6 +46,7 @@ export default class Preloader extends Phaser.Scene {
       repeat: -1,
     });
 
+    // call game scene after every asset has been loaded
     this.scene.start(SceneKeys.Game);
   }
 }
