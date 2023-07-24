ddbe7873afbbea03f757d884025b1651b4b885bc
Lettuce05 <romaine@unlv.nevada.edu>
2023-07-24 13:15:36 -0600

    Add functionality to scroll background

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -4,6 +4,8 @@ import SceneKeys from "../consts/SceneKeys";
 import AnimationKeys from "../consts/AnimationKeys";
 
 export default class Game extends Phaser.Scene {
+  private background!: Phaser.GameObjects.TileSprite;
+
   constructor() {
     super(SceneKeys.Game);
   }
@@ -13,17 +15,32 @@ export default class Game extends Phaser.Scene {
   create() {
     const width = this.scale.width;
     const height = this.scale.height;
-    this.add
+    this.background = this.add
       .tileSprite(0, 0, width, height, TextureKeys.Background)
-      .setOrigin(0);
+      .setOrigin(0)
+      .setScrollFactor(0, 0);
 
-    this.add
+    const mouse = this.physics.add
       .sprite(
         width * 0.5,
-        height * 0.5,
+        height - 30,
         TextureKeys.RocketMouse,
         "rocketmouse_fly01.png",
       )
+      .setOrigin(0.5, 1)
       .play(AnimationKeys.RocketMouseRun);
+
+    const body = mouse.body as Phaser.Physics.Arcade.Body;
+    body.setCollideWorldBounds(true);
+    body.setVelocityX(200);
+
+    this.physics.world.setBounds(0, 0, Number.MAX_SAFE_INTEGER, height - 30);
+
+    this.cameras.main.startFollow(mouse);
+    this.cameras.main.setBounds(0, 0, Number.MAX_SAFE_INTEGER, height);
+  }
+
+  update(time: number, delta: number): void {
+    this.background.setTilePosition(this.cameras.main.scrollX);
   }
 }
